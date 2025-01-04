314cbdf8534e3972c1924b5edd5f6e1d8cf9280a
Mayank Kumar <mayankgupta1327@gmail.com>
2025-01-04 19:13:10 +0530

    Delete Communication in both ways/testChildComponentParentToChildCommunication.js

--- Communication in both ways/testChildComponentParentToChildCommunication.js
+++
@@ -1,7 +0,0 @@
-import { LightningElement, api } from 'lwc';
-
-export default class TestChildComponentParentToChildCommunication extends LightningElement {
-    @api display;
-    @api message;
-
-}
